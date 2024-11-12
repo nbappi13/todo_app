ad34af9f3ee437488a570320a336d382913b5bf5
Md. Nahid Hasan Bappi <nahidbappi13@gmail.com>
2024-11-12 23:14:19 +0600

    todo issue solved in form

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import Header from "./components/Header";
 import Todo from "./components/Todo";
 import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [todos, setTodos] = useState([]);
+
   return (
     <div className="App">
-      <Header></Header>
-      <Todo></Todo>
+      <Header />
+      <Todo todos={todos} setTodos={setTodos} />
     </div>
   );
 }

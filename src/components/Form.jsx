40761b89f8cd12cf82b2b90ee3566306df98bc39
Md. Nahid Hasan Bappi <nahidbappi13@gmail.com>
2024-11-13 12:23:36 +0600

    added some css styles in form file

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import styles from './form.module.css';
 
 export default function Form({ todos, setTodos }) {
   const [todo, setTodo] = useState("");
@@ -10,13 +11,16 @@ export default function Form({ todos, setTodos }) {
   }
 
   return (
-    <form onSubmit={handleSubmit}>
-      <input
+    <form className={styles.todoform} onSubmit={handleSubmit}>
+      <div className={styles.inputContainer}>
+      <input className={styles.modernInput}
         onChange={(e) => setTodo(e.target.value)}
         value={todo}
         type="text"
+        placeholder="Enter todo item"
       />
-      <button type="submit">Add</button>
+      <button className={styles.modernButton} type="submit">Add</button>
+      </div>
     </form>
   );
 }
